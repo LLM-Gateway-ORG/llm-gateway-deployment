227734f687aad69241a5b6c7293c57b4bf61cf3e
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-11-03 00:53:22 +0530

    updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-docker stack deploy --compose-file deploy-stack.yml llm-gateway
-echo "[+] Finished deploying the stack"
+# Remove the existing stack
+docker stack rm llm-gateway
+echo "[+] Removed existing stack: llm-gateway"
+
+# Wait for the stack to be fully removed
+echo "[*] Waiting for services and containers to be fully stopped..."
+sleep 10  # Adjust the sleep time as needed
+
+# Deploy the new stack
+docker stack deploy --compose-file docker-stack.yml llm-gateway
+echo "[+] Finished deploying the stack: llm-gateway"
